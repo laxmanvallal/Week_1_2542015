249579790a1066510595325669ec62629b5cbcb4
laxmanvallal <laxmanvallal60@gmail.com>
2026-01-03 20:37:56 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,8 @@
+def circle(r):
+	 print("area of circle",3.14*r*r)
+def triangle(b,h):
+	print("area of triangle",0.5*b*h)
+def square(side):
+	print("area of side",side*side)
+def rectangle(l,b):
+	print("area of rectangle",l*b)
---
+++ main.py
@@ -0,0 +1,5 @@
+import area
+area.circle(2)
+area.triangle(10,3)
+area.square(3)
+area.rectangle(3,6)
